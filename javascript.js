5f2795dc408e030483af44a915dd5d4e5c9930f3
Jonathan Ibarra <scottcash20@gmail.com>
2022-05-14 14:30:27 -0500

    Edit javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
-console.log("hello_world");
+function computerPlay(choice = Math.floor(Math.random() * 3)) {
+
+if (choice = 0) {
+    return "Rock";
+} else if (choice = 1) {
+    return "Paper";
+} else if (choice = 2) {
+    return "Scissors";
+}
+
+return choice;
+}
+
+console.log(computerPlay());
