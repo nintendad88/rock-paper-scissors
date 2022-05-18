c00d1656c05ac1d436ee9380b28cf8774a296fe8
Jonathan Ibarra <scottcash20@gmail.com>
2022-05-18 15:58:26 -0500

    updated javascript.js

--- javascript.js
+++ javascript.js
@@ -1,9 +1,17 @@
 function computerPlay(throws) {
 
-return throws[Math.floor(Math.random()*throws.length)];
+    return throws[Math.floor(Math.random()*throws.length)];
 
 }
 
 const throws = ['Rock','Paper','Scissors'];
 
-console.log(computerPlay(throws));
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == 'Rock' || computerSelection == 'Rock' || computerSelection == 'Scissors' || computerSelection == 'Paper') {
+        alert( 'You won! Paper beats Rock!');
+    }   
+  }
+  
+  const playerSelection = prompt("Roshambo baby! Rock, Paper or Scissors??");
+  const computerSelection = computerPlay(throws);
+  console.log(playRound(playerSelection, computerSelection));
